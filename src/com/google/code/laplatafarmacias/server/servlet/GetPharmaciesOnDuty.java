8b77b285968ade8979133309e7b55c1655c6f129
epere4 <epere4@91825e46-5189-213a-ac77-1da15c046f9d>
2010-08-20 04:35:12 +0000

    Adding the servlet that reads from http://pipes.yahoo.com/epere4/farmaciasdeturnolaplatamapeadas and publishes in http://laplatafarmacias.appspot.com/laplatafarmacias/getPharmaciesOnDuty

---
+++ src/com/google/code/laplatafarmacias/server/servlet/GetPharmaciesOnDuty.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.google.code.laplatafarmacias.server.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URL;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author eduardo.pereda
+ * 
+ */
+@SuppressWarnings("serial")
+public class GetPharmaciesOnDuty extends HttpServlet {
+
+    /**
+     * This corresponds to
+     * http://pipes.yahoo.com/epere4/farmaciasdeturnolaplatamapeadas
+     */
+    public static final String PHARMACIES_ON_DUTY_LOCATION = "http://pipes.yahoo.com/pipes/pipe.run?_id=89053b69288b8aef14c44a916f33a206&_render=rss";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	// http://code.google.com/appengine/docs/java/urlfetch/overview.html
+
+	URL url = new URL(PHARMACIES_ON_DUTY_LOCATION);
+	BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	String line;
+
+	resp.setContentType("text/xml");
+
+	PrintStream out = new PrintStream(resp.getOutputStream());
+
+	while ((line = reader.readLine()) != null) {
+	    out.println(line);
+	    // System.out.println("line:" + line);
+	}
+	reader.close();
+	out.flush();
+
+    }
+}
